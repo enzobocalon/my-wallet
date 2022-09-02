fb0b6326f5b08be1f2ed636465b8059b37d0434f
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-09-02 16:33:59 -0300

    feat: dashboard cc limit menu added

--- src/components/Dashboard/DashboardModules/TransactionsInfos/TransactionsInfos.tsx
+++ src/components/Dashboard/DashboardModules/TransactionsInfos/TransactionsInfos.tsx
@@ -1,45 +1,61 @@
-import React from 'react'
-import * as S from './style'
+import React, {useState} from "react";
+import * as S from "./style";
 
-import { LinearProgress } from '@mui/material'
-import Tooltip from '@mui/material/Tooltip'
+import { LinearProgress } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
 
-import {MdMoreVert} from 'react-icons/md'
-import Transactions from '../Transactions/Transactions'
+import { MdMoreVert } from "react-icons/md";
+import {FiEdit} from 'react-icons/fi'
+import Transactions from "../Transactions/Transactions";
 
 const TransactionsInfos = () => {
+
+  const [ccMenu, setCCMenu] = useState<boolean>(false);
+
   return (
     <>
-    <S.Container>
+      <S.Container>
         <S.FirstRow>
-            <S.CCLimit>
-                <S.CCLimitHeader>
-                  <h1>Monthly credit card limit</h1>
-                  <MdMoreVert size={20}/>
+          <S.CCLimit>
+            <S.CCLimitHeader>
+              <h1>Monthly credit card limit</h1>
+              <MdMoreVert size={20} id="more" onClick={() => {setCCMenu(prev => !prev)}}/>
 
-                  {/* CHANGE LIMIT MENU */}
-                </S.CCLimitHeader>
-                <Tooltip title={"10%"} placement="bottom">
-                  <LinearProgress variant="determinate" value={10} color={'inherit'} sx={{background: '#7A4CB3', padding: "0.3rem 0", borderRadius: '10px'}}/>
-                </Tooltip>
+              <S.LimitMenu showing = {ccMenu}>
                 <div>
-                  <span>0%</span>
-                  <span>100%</span>
+                  <FiEdit/>
+                  <span>Change Limit</span>
                 </div>
-            </S.CCLimit>
-          
+              </S.LimitMenu>
+            </S.CCLimitHeader>
+            <Tooltip title={"10%"} placement="bottom">
+              <LinearProgress
+                variant="determinate"
+                value={10}
+                color={"inherit"}
+                sx={{
+                  background: "#7A4CB3",
+                  padding: "0.3rem 0",
+                  borderRadius: "10px",
+                }}
+              />
+            </Tooltip>
+            <div>
+              <span>0%</span>
+              <span>100%</span>
+            </div>
+          </S.CCLimit>
         </S.FirstRow>
-
-    </S.Container>
-    <S.Container style={{"marginTop":"1rem"}}>
-      <S.TransactionsHeader>
-        <h1>Latest Transactions</h1>
-      </S.TransactionsHeader>
-      <Transactions />
-      <span id="viewTransactions">View transactions</span>
-    </S.Container>
+      </S.Container>
+      <S.Container style={{ marginTop: "1rem" }}>
+        <S.TransactionsHeader>
+          <h1>Latest Transactions</h1>
+        </S.TransactionsHeader>
+        <Transactions />
+        <span id="viewTransactions">View transactions</span>
+      </S.Container>
     </>
-  )
-}
+  );
+};
 
-export default TransactionsInfos
+export default TransactionsInfos;
--- src/components/Dashboard/DashboardModules/TransactionsInfos/style.ts
+++ src/components/Dashboard/DashboardModules/TransactionsInfos/style.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+interface IProps {
+    showing: boolean
+}
+
+
 export const Container = styled.div`
     background-color: #2E313E;
     width: 100%;
@@ -56,16 +61,67 @@ export const CCLimitHeader = styled.div`
     justify-content: space-between;
     align-items: center;
     margin-bottom: .3rem;
+    position: relative;
     span{
         font-size: 1rem !important;
         margin-top: 0 !important;
     }
-    svg{
-        cursor: pointer
+    #more{
+        transition: all .3s ease;
+        cursor: pointer;
     }
+
+    #more:hover{
+        background-color: #272836;
+        border-radius: 50%;
+        transition: all .3s ease;
+    }
+
 `
 
+export const LimitMenu = styled.div<IProps>`
+    position: absolute;
+    background-color: #1E1C2C;
+    width: fit-content;
+    padding: .6rem;
+    height: 45px;
+    right: 6px;
+    top: 29px;
+    z-index: 10;
+    border-radius: 10px 0 10px 10px;
+    opacity: ${props => props.showing ? '1' : '0'};
+    pointer-events: ${props => props.showing ? 'all' : 'none'};
+    transition: all .3s ease;
+    cursor: pointer;
 
+    div {
+        display: flex;
+        align-items: center;
+        gap: .5rem;
+        padding: .3rem .5rem;
+        transition: all .3s ease;
+    }
+
+    &:after{
+        content: '';
+        position: absolute;
+        display: block;
+        border-color: transparent transparent #1E1C2C transparent;
+        width: 0;
+        border-style: solid;
+        border-width: 0 5px 5px 5px;
+        height: 0;
+        right: 0;
+        top: -4px;
+    }
+
+    &:hover > div{
+        background-color: #272836;
+        border-radius: 10px;
+        transition: all .3s ease;
+    }
+    
+`
 
 export const TransactionsHeader = styled.div`
     width: 100%;
